a5768f4779a20329aced21aed5d325ea28989cea
Fanie Reynders <fanie@reynders.co>
2016-08-10 07:05:09 +0200

    added csharp generator

---
+++ src/WebApiproxy.Clients.CSharp/CSharpGenerator.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApiProxy.Clients.CSharp
+{
+    using System.Threading.Tasks;
+    using Core.Infrastructure;
+    using Core.Models;
+
+    public partial class CSharpGenerator : GeneratorBase
+    {
+        public CSharpGenerator()
+        {
+
+        }
+
+        public CSharpGenerator(Metadata metadata) : base(metadata)
+        {
+
+        }
+
+        public override async Task<string> Process()
+        {
+            return await Task.Run(() =>
+            {
+                return this.transformText();
+            });
+
+        }
+
+
+
+
+    }
+}
